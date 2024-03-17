349d6187394f35da5e44ca25a5646ce9bf2c7c24
Dominik Kies <dominik@kies-online.com>
2024-03-17 09:34:19 +0100

    add wait time before unmount, maybe it fix the unmount issues. And add some qoutes to the mount command

--- server/stick.sh
+++ server/stick.sh
@@ -2,8 +2,12 @@
 
 MOUNT_PATH="/var/www/html/images"
 JSON_PATH="/var/www/html/bilder.json"
+LOG_PATH="/home/jfw/stick.log"
 
 if [ "$1" == "remove" ]; then
+    echo "triggered USB remove, wait 5 seconds." >> $LOG_PATH
+    sleep 5
+    echo "... now try to unmont the stick." >> $LOG_PATH
     # Leeren der JSON-Datei, wenn der Stick entfernt wird
     echo "{\"bilder\": []}" > $JSON_PATH
     # Entfernen des Mount-Points
@@ -13,7 +17,7 @@ else
     DEVICE=$(lsblk -rpo "name,type" | awk '$2=="part" {print $1}' | head -1)
     mkdir -p $MOUNT_PATH
     # mount for user www-data -> uid=33,gid=33
-    mount $DEVICE $MOUNT_PATH -o uid=33,gid=33,ro
+    mount "$DEVICE $MOUNT_PATH -o uid=33,gid=33,ro"
     cd $MOUNT_PATH
     FILES=(*.jpg)
     echo "{\"bilder\": [" > $JSON_PATH
